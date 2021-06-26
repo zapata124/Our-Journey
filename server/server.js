6ef64a342d935ccaa67a8c829b002d7f81918f4a
Kadenj117 <kadenj117@gmail.com>
2021-06-26 15:50:01 -0600

    hello

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(express.json()); //testing
 
 //const apiRouter = require('./routes/api');
 
