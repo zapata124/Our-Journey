db2f3cecd897cdad848908c0b3f3f50251b6f944
prasad-pul <prasad.pul@gmail.com>
2021-07-01 18:38:05 -0400

    Final Commit before Dinner

--- client/Components/MainListItems.jsx
+++ client/Components/MainListItems.jsx
@@ -4,18 +4,20 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import PersonIcon from '@material-ui/icons/Person';
 import MapIcon from '@material-ui/icons/Map';
+import { useHistory } from 'react-router-dom';
 
 export const MainListItems = (props) => {
+  let history = useHistory();
 
   return (
     <div>
-      <ListItem button onClick={() => history.push('/dashboard/Biopage')}>
+      <ListItem button>
         <ListItemIcon>
           <PersonIcon />
         </ListItemIcon>
         <ListItemText primary="Bio" />
       </ListItem>
-      <ListItem button onClick={() => history.push('/dashboard/MapPage')}>
+      <ListItem button>
         <ListItemIcon>
           <MapIcon />
         </ListItemIcon>
