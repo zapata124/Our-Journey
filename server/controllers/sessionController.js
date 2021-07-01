35355ffc9a9d4137d051c8271bd93be52826e82e
Kadenj117 <kadenj117@gmail.com>
2021-07-01 13:04:46 -0600

    fixed problems with review creation and get request

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -4,11 +4,12 @@ const sessionController = {
   sessionStart(req, res, next) {
     console.log('start session here')
 
-    Session.create({ cookieId: res.locals.user.id }, (err, result) => {
+    Session.create({ cookieId: res.locals.user.id, username: res.locals.user.username }, (err, result) => {
       if (err) {
         console.log(err);
         return next(err);
       }
+      
       return next();
     })
   },
@@ -20,6 +21,7 @@ const sessionController = {
         console.log(err)
         return next(err);
       }
+      res.locals.username = result;
       return next()
     })
   }
--- server/model/sessionModel.js
+++ server/model/sessionModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema({
+  username: { type: String, require: true },
   cookieId: { type: String, require: true, unique: true },
   createdAt: { type: Date, expires: 3000, default: Date.now },
 });
