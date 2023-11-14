958174f98a90637c32d24b9e250766a184f5abd8
eddy zapata <ecz001@live.com>
2023-11-14 12:41:19 -0500

    removing all files to merge updated branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# Our-Journey
-Shared Travel blog to see where your friend have been and what they thought.
-can you all see this new message
--- client/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-//const React = require('react')
-import { render } from 'react-dom';
-//const { render } = require('react-dom');
-import App from './App';
-//const App = require('./client/App');
-
-render(<App />, document.querySelector('#app'));
